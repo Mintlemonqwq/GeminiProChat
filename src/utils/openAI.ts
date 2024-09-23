44726c1f97c9200605a083ffe56143ad7be5b297
Mintlemonqwq <1722785062@qq.com>
2024-09-24 04:40:27 +0800

    FIX ERROR
    FIX ERROR

--- src/utils/openAI.ts
+++ src/utils/openAI.ts
@@ -23,7 +23,8 @@ export const startChatAndSendMessageStream = async(history: ChatMessage[], newMe
       {category: 'HARM_CATEGORY_HARASSMENT', threshold: 'BLOCK_NONE'},
       {category: 'HARM_CATEGORY_HATE_SPEECH', threshold: 'BLOCK_NONE'},
       {category: 'HARM_CATEGORY_SEXUALLY_EXPLICIT', threshold: 'BLOCK_NONE'},
-      {category: 'HARM_CATEGORY_DANGEROUS_CONTENT', threshold: 'BLOCK_NONE'}
+      {category: 'HARM_CATEGORY_DANGEROUS_CONTENT', threshold: 'BLOCK_NONE'},
+      {category: 'HARM_CATEGORY_CIVIC_INTEGRITY', threshold: 'BLOCK_NONE'}
       ],
   })
 
